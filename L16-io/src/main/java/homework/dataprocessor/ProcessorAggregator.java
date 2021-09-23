e452667f6aa87cca08f9211574c0915dbba4d429
Timur Gainitdinov <gainitdinovtimur123@gmail.com>
2021-09-23 19:38:16 +0500

    Homework 16

--- L16-io/src/main/java/homework/dataprocessor/ProcessorAggregator.java
+++ L16-io/src/main/java/homework/dataprocessor/ProcessorAggregator.java
@@ -1,10 +1,9 @@
 package homework.dataprocessor;
 
 import homework.model.Measurement;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
@@ -13,9 +12,8 @@ public class ProcessorAggregator implements Processor {
     public Map<String, Double> process(List<Measurement> data) {
         //группирует выходящий список по name, при этом суммирует поля value
         return data.stream()
-            .sorted(Comparator.comparing(Measurement::getName))
             .collect(Collectors
-                .groupingBy(Measurement::getName, LinkedHashMap::new,
+                .groupingBy(Measurement::getName, TreeMap::new,
                     Collectors.summingDouble(Measurement::getValue)));
     }
 }
